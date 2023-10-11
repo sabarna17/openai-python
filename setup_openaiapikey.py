385c677308a167d96a329bdf0b602cf2714dff84
sabarna17 <39834671+sabarna17@users.noreply.github.com>
2023-10-11 13:49:53 +0530

    Create setup_openaiapikey.py

---
+++ setup_openaiapikey.py
@@ -0,0 +1,34 @@
+# Open AI API key
+import os
+from getpass import getpass
+import openai
+
+os.environ['OPENAI_API_KEY'] = 'sdsd'
+def testOpenAIKey(openaiapikey):
+    openai.api_key = openaiapikey #'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'
+    prompt = "What is the calculated value for 6 * 12"
+    model = "text-davinci-003"
+    try: 
+        response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=50)
+        generated_text = response.choices[0].text
+    except:
+        generated_text = 'oops'
+    if (generated_text.find('72') < 0):
+        print('Your OpenAI API Key is wrong')
+        askForOpenAIKey()
+    print('Your OpenAI API Key is active')
+def askForOpenAIKey():
+    openai_api_key = getpass("Enter your OpenAI API key: ")
+    os.environ['OPENAI_API_KEY'] = openai_api_key
+    print('You have entered: ', openai_api_key)
+    testOpenAIKey(openai_api_key)
+    if(int(testOpenAIKey(openai_api_key)) > 0):
+        askForOpenAIKey()
+
+def checkKey(dic, key):
+    if key in dic.keys():
+        testOpenAIKey(dic[key])
+    else:
+        askForOpenAIKey()
+
+checkKey(os.environ, 'OPENAI_API_KEY')
